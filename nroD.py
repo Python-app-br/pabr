f1a1801794b3ac9a3dcb0dd3d12b9eef9e9306bf
Python-app-br <python.app.br@gmail.com>
2021-10-10 14:09:04 -0300

    up 2

--- nroD.py
+++ nroD.py
@@ -71,9 +71,78 @@ que não são ignorados pelo arquivo .gitignore à "área de teste".
 git add -A
 
 
-''''''
+'''checar status'''
+git status
+
+'''tudo ok. envie os arquivos para seu repositório'''
+git commit -m "First version of application moved into github"
+
+'''sincronizar enviando repo local para o repo remoto github'''
+
+git push origin master
+
+'''fazer backup do projeto'''
+
+'''configurar app django para o heroku'''
+'''criar arquivo sem extenção Procfile na raiz do git repo local'''
+'''adicionar ao Procfile'''
+
+web: gunicorn locallibrary.wsgi --log-file -
+
+'''instalar gunicorn'''
+pip3 install gunicorn
+
+'''instalar dj-database-url'''
+pip3 install dj-database-url
+
+'''configurar database -- settings.py'''
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+'''usaremos SQLite mas é possivelinstalar as dependencias Postgres localmente'''
+
+sudo apt-get install python-pip python-dev libpq-dev postgresql postgresql-contrib
+pip3 install psycopg2-binary
+
+'''configurar variaveis settings.py'''
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
 
 
+'''instalar whitenoise'''
+pip3 install whitenoise
+
+'''configurar MIDDLEWARE e adicione o WhiteNoiseMiddleware abaixo de SecurityMiddleware:'''
+
+'whitenoise.middleware.WhiteNoiseMiddleware',
+
+
+'''diminuir tamanho dos arquivos estaticos '''
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+'''criar requirements.txt'''
+pip3 freeze > requirements.txt
+'''conteudo'''
+dj-database-url==0.5.0
+Django==2.1.5
+gunicorn==19.9.0
+psycopg2-binary==2.7.7
+whitenoise==4.1.2
+
+'''criar arquivo na raiz runtime.txt'''
+python-3.7.0
 
 
 
