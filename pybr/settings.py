10de82ba1c89cd9c63e983f14f84290f3f5c5c0b
Python-app-br <python.app.br@gmail.com>
2021-10-11 11:56:23 -0300

    hero23

--- pybr/settings.py
+++ pybr/settings.py
@@ -18,22 +18,17 @@ from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-dpm*apqp=oltkdj8m=&6ahmzo*kw+j-^9_ykkeod&)m%25osdq'
-SECRET_KEY = os.environ.get('SECRET_KEY')
-#SECRET_KEY = config('SECRET_KEY')
-#SECRET_KEY = os.environ['SECRET_KEY']
-# SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-#DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-#DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = os.environ.get('DEBUG') == True
-#ALLOWED_HOSTS = []
+
+#DEBUG = os.environ.get('DEBUG') == True
+
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost' , 'https://afternoon-oasis-86204.herokuapp.com/']
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
@@ -94,6 +89,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
