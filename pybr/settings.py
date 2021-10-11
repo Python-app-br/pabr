7e55d45d08fef4ea44a399bb15b5540c654376bf
Python-app-br <python.app.br@gmail.com>
2021-10-11 13:15:02 -0300

    hero38

--- pybr/settings.py
+++ pybr/settings.py
@@ -17,7 +17,9 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ['SECRET_KEY']
+
+SECRET_KEY = config('SECRET_KEY', default='valor padrão, caso não exista essa variável de ambiente')
+#SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = config('DEBUG', cast=int, default=False)
 
 
