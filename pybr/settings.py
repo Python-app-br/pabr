f3a59b4b094a07c76f1fa1a2aeda855cbd477d00
Python-app-br <python.app.br@gmail.com>
2021-10-11 14:49:36 -0300

    hero56

--- pybr/settings.py
+++ pybr/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 #DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', cast=bool)
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost' , 'https://afternoon-oasis-86204.herokuapp.com/']
