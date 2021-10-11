47f1caed91c305bc9af0c80e4b8fa5e9ebc9faed
Python-app-br <python.app.br@gmail.com>
2021-10-11 13:12:55 -0300

    hero37

--- pybr/settings.py
+++ pybr/settings.py
@@ -10,28 +10,19 @@ import os
 import dj_database_url
 from pathlib import Path
 import django_heroku
-from decouple import config, Csv
+from decouple import config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ['SECRET_KEY']
+DEBUG = config('DEBUG', cast=int, default=False)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dpm*apqp=oltkdj8m=&6ahmzo*kw+j-^9_ykkeod&)m%25osdq'
-
-
-#SECRET_KEY = os.environ['SECRET_KEY']
-# SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-#ALLOWED_HOSTS = []
 
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost' , 'https://afternoon-oasis-86204.herokuapp.com/']
-
 
 
 
