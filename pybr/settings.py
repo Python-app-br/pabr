83b3e42c97ec4bd305a47a71ca5d34c82e4028e0
Python-app-br <python.app.br@gmail.com>
2021-10-11 12:54:24 -0300

    hero334

--- pybr/settings.py
+++ pybr/settings.py
@@ -142,8 +142,8 @@ USE_TZ = True
 # Heroku: Update database configuration from $DATABASE_URL.
 
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
